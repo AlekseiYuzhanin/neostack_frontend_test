d3957f5ca2b8f5002c5075c072ecf60c9c7fa47f
AlekseiYuzhanin <ZombeHalk@gmail.com>
2023-08-23 18:33:41 +0700

    date adding commit

--- src/components/MainPage.tsx
+++ src/components/MainPage.tsx
@@ -2,13 +2,15 @@ import { FC,useState } from "react"
 import SliderComponent from "./YearSliderComponent"
 
 
-const MainPage:FC = () =>{
-    return (
-      <div className="MainPage">
-        <SliderComponent min={2014} max={2021}/>
-      </div>
+const MainPage: React.FC = () => {
+  const startDate = new Date('2010-01-01'); // Установите нужную начальную дату
+  const endDate = new Date('2021-12-31'); // Установите нужную конечную дату
 
+  return (
+    <div className="MainPage">
+      <SliderComponent startDate={startDate} endDate={endDate} />
+    </div>
   );
-}
+};
 
 export default MainPage
--- src/components/YearSliderComponent.tsx
+++ src/components/YearSliderComponent.tsx
@@ -1,40 +1,52 @@
-import React, { useState } from 'react';
+import React from 'react';
 import Slider from 'antd/lib/slider';
-import '../styles/SliderComponent/SliderComponent.css'
-import MonthSliderComponent from './MonthSliderComponent';
+import '../styles/SliderComponent/SliderComponent.css';
+import { start } from 'repl';
 
 type RangeSliderProps = {
-    min: number;
-    max: number;
-  };
-  
-const SliderComponent: React.FC<RangeSliderProps> = ({min,max}) => {
-  const [value, setValue] = useState<[number, number]>([min, max]);
+  startDate: Date;
+  endDate: Date;
+};
 
+const SliderComponent: React.FC<RangeSliderProps> = ({ startDate, endDate }) => {
   const handleChange = (newValue: [number, number]) => {
-    setValue(newValue);
+    // Обработка изменений значения слайдера
   };
 
-const marks: { [key: number]: string } = {};
-for (let year = min; year <= max; year++) {
+  const tipFormatter = (value: any): React.ReactNode => {
+    const year = startDate.getFullYear() + Math.floor(value / 12);
+    const month = (startDate.getMonth() + value) % 12;
+    const monthName = new Intl.DateTimeFormat('ru', { month: 'long' }).format(new Date(year, month));
+    return <>
+    <div style={{display: "flex", flexDirection: "column", textAlign: "center"}}>
+      <div>
+        {monthName}
+      </div>
+      <div>
+        {year}
+      </div>
+    </div></>;
+  };
+
+  const marks: { [key: number]: string } = {};
+  for (let year = startDate.getFullYear(); year <= endDate.getFullYear(); year++) {
     marks[year] = year.toString();
   }
 
   return (
-    <div style={{paddingTop: "100px", padding: "100px"}}>
+    <div style={{ paddingTop: '100px', padding: '100px' }}>
       <h2>Слайдер года</h2>
       <Slider
-        range
-        defaultValue={[min, max]}
-        min={min}
-        max={max}
-        onChange={handleChange}
         marks={marks}
-        className='SliderComponent'
+        range
+        defaultValue={[0,56]}
+        min={0}
+        max={(endDate.getFullYear() - startDate.getFullYear()) * 12 + endDate.getMonth() - startDate.getMonth()}
+        onChange={handleChange}
+        className="SliderComponent"
         tooltipVisible={true}
+        tipFormatter={tipFormatter}
       />
-      <h2>Слайдер месяца</h2>
-      <MonthSliderComponent/>
     </div>
   );
 };
